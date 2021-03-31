e37a448cc3daa9a470a17d4edad7ac9a626d9996
Abby Isaacson <oabby1@yahoo.com>
2021-03-31 10:46:45 -0700

    Initial commit

--- github_setup.R
+++ github_setup.R
@@ -1,16 +1,20 @@
 install.packages("usethis")
 library(usethis)
-#library("gitcreds")
+
 
 edit_git_config()
 
 create_github_token()
-#0c103cfef826e1e6927722d79fe3663b0767a0f8
+
+
+
+install.packages("gitcreds")
+library("gitcreds")
+
 gitcreds_set()
 
 # make a repo
 
-use_git()
 
 #send to github
 
